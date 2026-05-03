d58965c4cb03e768a8152ee6dceafda0d3b6c886
Jakub Król <kozak10555@gmail.com>
2026-05-03 21:00:29 +0200

    init scrapper

---
+++ scrapper/price-nest-scrapper/app.js
@@ -0,0 +1,17 @@
+const { chromium } = require('playwright');
+
+(async () => {
+  console.log("Init...");
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+  
+  
+  const url = 'https://www.scrapethissite.com/pages/simple/'; 
+  
+  await page.goto(url);
+  const title = await page.title();
+  
+  console.log(`Connected: ${title}`);
+  
+  await browser.close();
+})();
