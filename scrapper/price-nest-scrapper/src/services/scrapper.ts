8679367e19dbf5bd47db77d58f0a550d316eba37
Jakub Król <kozak10555@gmail.com>
2026-05-07 21:55:46 +0200

    handled scrapping for dynamically loading pages

--- scrapper/price-nest-scrapper/src/services/scrapper.ts
+++ scrapper/price-nest-scrapper/src/services/scrapper.ts
@@ -1,25 +1,37 @@
 import { chromium } from "playwright";
 
 export default async function scrapeCeneo(productName: string) {
-    const browser = await chromium.launch({ headless: true });
+    const browser = await chromium.launch({ headless: true});
     const context = await browser.newContext({
         userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
     });
     const page = await context.newPage();
 
     try {
+        productName = productName.toUpperCase()
         console.log(`Scraping Ceneo for product: ${productName}`);
         await page.goto(`https://www.ceneo.pl/;szukaj-${encodeURIComponent(productName)}`, { waitUntil: "domcontentloaded" });
 
-        const products = await page.$$eval('.cat-prod-row', (items) => {
+        try {
+            await page.waitForSelector('.cat-prod-row__body', { timeout: 5000 });
+        } catch (e) {
+            console.log("Nie znaleziono standardowych wierszy produktów.");
+        }
+
+        await page.mouse.wheel(0, 1000)
+        await page.waitForTimeout(500)
+
+        const products = await page.$$eval('.cat-prod-row__body, .cat-prod-row', (items) => {
+            console.log("Founded")
             return items.map(item => {
-                const title = item.querySelector('.cat-prod-row__name')?.textContent?.trim();
+                const title = item.querySelector('.cat-prod-row__name, .cat-prod-row__desc')?.textContent?.trim();
+                //const title = item.querySelector('.cat-prod-row__content')?.textContent?.trim();
                 const price = item.querySelector('.price')?.textContent?.trim();
                 return { title, price };
             }).filter(p => p.title && p.price);
         });
 
-        console.table(products);
+        //console.table(products);
 
         return products;
 
@@ -27,6 +39,7 @@ export default async function scrapeCeneo(productName: string) {
         console.error("Error occurred:", error);
     } finally {
         console.log("Success");
+        //await page.pause()
         await browser.close();
     }
 
